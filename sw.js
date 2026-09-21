ea82633d7a1b957f376069e2edfa412293ec9a36
Krzysztof Funkowski <69473574+Funiek@users.noreply.github.com>
2026-09-21 13:13:22 +0200

    Switch GymPath to manual dataset import and retain all imported regions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gympath-shell-v3';
+const CACHE='gympath-shell-v4';
 const APP=['./','./index.html','./style.css','./core.js','./importer.js','./app.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
